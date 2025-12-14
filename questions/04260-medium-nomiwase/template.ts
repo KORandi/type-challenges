702aadcd62d6ed04d4e1edff40855f0aa033548c
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-14 18:46:30 +0100

    04260

--- questions/04260-medium-nomiwase/template.ts
+++ questions/04260-medium-nomiwase/template.ts
@@ -1,1 +1,16 @@
-type AllCombinations<S> = any
+type AllCombinations<
+  S extends string,
+  T extends string = S,
+> = S extends `${infer L}${infer R}`
+  ?
+  | `${L}${AllCombinations<R>}`
+  | (`${R}${L}` extends T
+    ? ''
+    : AllCombinations<`${R}${L}`, T>)
+  : ''
+
+type X1 = AllCombinations<''>
+type X2 = AllCombinations<'A'>
+type X3 = AllCombinations<'AB'>
+type X4 = AllCombinations<'ABC'>
+type X5 = AllCombinations<'ABCD'>
