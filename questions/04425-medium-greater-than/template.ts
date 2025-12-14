c6e4bb5425d6755775ab6b0c49d98999f66501b6
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-14 19:49:46 +0100

    04425

--- questions/04425-medium-greater-than/template.ts
+++ questions/04425-medium-greater-than/template.ts
@@ -1,1 +1,8 @@
-type GreaterThan<T extends number, U extends number> = any
+type GreaterThan<T extends number, U extends number, Counter extends Array<unknown> = []> =
+Counter['length'] extends T
+  ? false
+  : Counter['length'] extends U
+    ? true
+    : GreaterThan<T, U, [unknown, ...Counter]>
+
+type X = GreaterThan<1, 0>
