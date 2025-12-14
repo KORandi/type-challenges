fed82fdb268935420b069826b6855afdc0db3a03
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-14 17:43:02 +0100

    Fibonacci

--- questions/04179-medium-flip/template.ts
+++ questions/04179-medium-flip/template.ts
@@ -1,1 +1,8 @@
-type Flip<T> = any
+type Flip<T extends Record<PropertyKey, PropertyKey | boolean>> =
+{
+  [P in keyof T as T[P] extends PropertyKey
+    ? T[P]
+    : T[P] extends boolean
+      ? `${T[P]}`
+      : never]: P
+}
--- questions/04182-medium-fibonacci-sequence/template.ts
+++ questions/04182-medium-fibonacci-sequence/template.ts
@@ -1,1 +1,8 @@
-type Fibonacci<T extends number> = any
+type Fibonacci<
+  T extends number,
+  CurrentIndex extends unknown[] = [unknown],
+  Prev extends unknown[] = [],
+  Current extends unknown[] = [unknown],
+> = CurrentIndex['length'] extends T
+  ? Current['length']
+  : Fibonacci<T, [...CurrentIndex, unknown], Current, [...Prev, ...Current]>
