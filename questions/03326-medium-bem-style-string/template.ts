672a5b520bf89a9f4ffa49323bafcd249e531f54
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-14 14:38:45 +0100

    finish 03326 - bem styling

--- questions/03326-medium-bem-style-string/template.ts
+++ questions/03326-medium-bem-style-string/template.ts
@@ -1,1 +1,4 @@
-type BEM<B extends string, E extends string[], M extends string[]> = any
+type ArrayToUnionPrefix<U extends string, T extends readonly string[]> = [T[number]] extends [never] ? '' : `${U}${T[number]}`
+
+type BEM<B extends string, E extends string[], M extends string[]> =
+  `${B}${ArrayToUnionPrefix<'__', E>}${ArrayToUnionPrefix<'--', M>}`
