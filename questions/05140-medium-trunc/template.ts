1abbf76fd262d066b70b46d743355be9d4057cbf
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-15 18:19:32 +0100

    05140 medium

--- questions/05140-medium-trunc/template.ts
+++ questions/05140-medium-trunc/template.ts
@@ -1,1 +1,7 @@
-type Trunc = any
+type Trunc<T extends number | string, Agr extends string = ''> = `${T}` extends `${infer L}${infer R}`
+  ? L extends '.'
+    ? Agr extends '' | '-'
+      ? `${Agr}0`
+      : Agr
+    : Trunc<R, `${Agr}${L}`>
+  : Agr
