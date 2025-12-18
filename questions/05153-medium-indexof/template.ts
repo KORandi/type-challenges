8b799b236f6cfe56ccb15d5cf30cad9684de3cf9
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-19 00:47:54 +0100

    05153

--- questions/05153-medium-indexof/template.ts
+++ questions/05153-medium-indexof/template.ts
@@ -1,1 +1,6 @@
-type IndexOf<T, U> = any
+type IndexOf<T extends readonly unknown[], U, C extends unknown[] = []> =
+  T extends [infer L, ...infer R extends readonly unknown[]]
+    ? IsEqual<L, U> extends true
+      ? C['length']
+      : IndexOf<R, U, [...C, unknown]>
+    : -1
