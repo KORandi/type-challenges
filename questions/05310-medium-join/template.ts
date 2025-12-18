7cdae622690de4c5dd8aadeb3d29a4250146e444
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-19 00:58:34 +0100

    5310 join

--- questions/05310-medium-join/template.ts
+++ questions/05310-medium-join/template.ts
@@ -1,1 +1,6 @@
-type Join<T, U> = any
+type Join<T extends string[], U extends string | number = ','> =
+T extends [infer L extends string, ...infer R extends string[]]
+  ? R['length'] extends 0
+    ? L
+    : `${L}${U}${Join<R, U>}`
+  : ''
