066c7214f96b469e0685b819db40f06fcc84f96b
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-19 02:23:00 +0100

    Map types

--- questions/05821-medium-maptypes/template.ts
+++ questions/05821-medium-maptypes/template.ts
@@ -1,1 +1,8 @@
-type MapTypes<T, R> = any
+type MapTypes<T, R extends { mapFrom: unknown, mapTo: unknown }> =
+{
+  [K in keyof T]: T[K] extends R['mapFrom']
+    ? R extends { mapFrom: T[K] }
+      ? R['mapTo']
+      : never
+    : T[K]
+}
