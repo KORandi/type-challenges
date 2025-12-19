2d19d8346952f0662ac1fc8209ef4b5118bedc86
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-19 23:38:43 +0100

    08767 combination

--- questions/08767-medium-combination/template.ts
+++ questions/08767-medium-combination/template.ts
@@ -1,1 +1,6 @@
-type Combination<T extends string[]> = any
+type Combination<T extends string[], All = T[number], Item = All> =
+Item extends string
+  ? Item | `${Item} ${Combination<[], Exclude<All, Item>>}`
+  : never
+
+type X = Combination<['foo', 'bar', 'baz']>
