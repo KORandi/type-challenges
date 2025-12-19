7fdf64952d067547fd5be5b02dd3ae0abfaadd18
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-19 01:10:01 +0100

    05317 lastindexof

--- questions/05317-medium-lastindexof/template.ts
+++ questions/05317-medium-lastindexof/template.ts
@@ -1,1 +1,6 @@
-type LastIndexOf<T, U> = any
+type LastIndexOf<T extends unknown[], U, Result extends number = -1, C extends unknown[] = []> =
+T extends [infer L, ...infer R]
+  ? IsEqual<L, U> extends true
+    ? LastIndexOf<R, U, C['length'], [...C, unknown]>
+    : LastIndexOf<R, U, Result, [...C, unknown]>
+  : Result
