2dc4347cdaf07c22c0740c5b1acebfbf86bae989
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-19 01:31:45 +0100

    05360-unique

--- questions/05360-medium-unique/template.ts
+++ questions/05360-medium-unique/template.ts
@@ -1,1 +1,6 @@
-type Unique<T> = any
+type Unique<T extends unknown[], Res extends unknown[] = []> =
+T extends [infer L, ...infer R]
+  ? IndexOf<Res, L> extends -1
+    ? Unique<R, [...Res, L]>
+    : Unique<R, Res>
+  : Res
