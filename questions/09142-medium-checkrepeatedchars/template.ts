af308409c15bb1e2a45c4f377b43758d2deb65e6
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-20 00:05:40 +0100

    09142 Check repeated chars

--- questions/09142-medium-checkrepeatedchars/template.ts
+++ questions/09142-medium-checkrepeatedchars/template.ts
@@ -1,1 +1,6 @@
-type CheckRepeatedChars<T extends string> = any
+type CheckRepeatedChars<T extends string, Res extends string = ''> =
+T extends `${infer L}${infer R}`
+  ? L extends Res
+    ? true
+    : CheckRepeatedChars<R, Res | L>
+  : false
