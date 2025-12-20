a17c4da143ba9e4ec103bceb4e12bd974854f834
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-20 21:43:03 +0100

    25270 - transpone

--- questions/25270-medium-transpose/template.ts
+++ questions/25270-medium-transpose/template.ts
@@ -1,1 +1,14 @@
-type Transpose<M extends number[][]> = any
+type Transpose<M extends number[][]> =
+M[0] extends infer M0 extends Array<number>
+  ? { [K in keyof M0]: M extends infer MK extends Array<number[]>
+      ?
+        {
+          [J in keyof MK]: J extends keyof M
+            ? K extends keyof M[J]
+              ? M[J][K]
+              : never
+            : never
+        }
+      : never
+    }
+  : []
