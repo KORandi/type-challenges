7214638d10e30944356573567d0b7c6c241b9788
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-20 14:06:25 +0100

    09989 record map counter

--- questions/09989-medium-tong-ji-shu-zu-zhong-de-yuan-su-ge-shu/template.ts
+++ questions/09989-medium-tong-ji-shu-zu-zhong-de-yuan-su-ge-shu/template.ts
@@ -1,1 +1,18 @@
-type CountElementNumberToObject<T> = any
+type CountElementNumberToObject<T extends unknown[], Agr extends Record<PropertyKey, unknown[]> = {}, TT = Flatten<T>> =
+Includes<T, never> extends true
+  ? {}
+  : TT extends [infer L extends PropertyKey, ... infer P]
+    ? L extends keyof Agr
+      ? CountElementNumberToObject<P, {
+        [K in keyof Agr]: Agr[K] extends unknown[]
+          ? L extends K
+            ? [...Agr[K], unknown]
+            : Agr[K]
+          : never
+      } >
+      : CountElementNumberToObject<P, Agr & { [K in L]: [unknown] }>
+    : {
+        [K in keyof Agr]: Agr[K] extends unknown[]
+          ? Agr[K]['length']
+          : never
+      }
