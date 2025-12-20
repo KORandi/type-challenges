f0c0aa33b87204fa05de53f5b5f963479f18236a
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-20 09:54:13 +0100

    09616 Parse url params

--- questions/04260-medium-nomiwase/template.ts
+++ questions/04260-medium-nomiwase/template.ts
@@ -8,9 +8,3 @@ type AllCombinations<
     ? ''
     : AllCombinations<`${R}${L}`, T>)
   : ''
-
-type X1 = AllCombinations<''>
-type X2 = AllCombinations<'A'>
-type X3 = AllCombinations<'AB'>
-type X4 = AllCombinations<'ABC'>
-type X5 = AllCombinations<'ABCD'>
--- questions/09616-medium-parse-url-params/template.ts
+++ questions/09616-medium-parse-url-params/template.ts
@@ -1,1 +1,6 @@
-type ParseUrlParams<T> = any
+type ParseUrlParams<T extends string> =
+T extends `${string}:${infer K}`
+  ? K extends `${infer L extends string}/${infer R extends string}`
+    ? L | ParseUrlParams<R>
+    : K
+  : never
