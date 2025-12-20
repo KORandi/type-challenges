a92366c4641013ded2fdb57a4515493b06afe038
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-20 11:52:49 +0100

    09898 find eles

--- questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
+++ questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
@@ -1,1 +1,6 @@
-type FindEles<T extends any[]> = any
+type FindEles<T extends any[], Visited extends unknown[] = [], Result extends unknown[] = []> =
+  T extends [infer L, ...infer R]
+    ? Includes<[...R, ...Visited], L> extends true
+      ? FindEles<R, [...Visited, L], Result>
+      : FindEles<R, [...Visited, L], [...Result, L]>
+    : Result
