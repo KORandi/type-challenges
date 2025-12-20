c279c48a3bad011e57657eaae20cc291ab74ab13
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-20 20:59:36 +0100

    Replace first

--- questions/21220-medium-permutations-of-tuple/template.ts
+++ questions/21220-medium-permutations-of-tuple/template.ts
@@ -4,5 +4,3 @@ T extends [infer L, ...infer R]
     ? [L, ...PermutationsOfTuple<R>] | [LL, ...PermutationsOfTuple<[L, ...Rest]>] | [...PermutationsOfTuple<[L, ...Rest]>, LL] | [...PermutationsOfTuple<R>, L]
     : [L, ...PermutationsOfTuple<R>] | [...PermutationsOfTuple<R>, L]
   : []
-
-type X = PermutationsOfTuple<[1, number, unknown]>
--- questions/25170-medium-replace-first/template.ts
+++ questions/25170-medium-replace-first/template.ts
@@ -1,1 +1,8 @@
-type ReplaceFirst<T extends readonly unknown[], S, R> = any
+type ReplaceFirst<T extends readonly unknown[], S, R> =
+T extends [infer L, ...infer Rest]
+  ? L extends S
+    ? [R, ...Rest]
+    : [L, ...ReplaceFirst<Rest, S, R>]
+  : []
+
+type X = ReplaceFirst<[1, 2, 3], 3, 4>
