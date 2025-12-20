a99b9cdda64c475218b81bd2bceea513779a7648
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-20 11:19:07 +0100

    09896 get middle element

--- questions/09896-medium-get-middle-element/template.ts
+++ questions/09896-medium-get-middle-element/template.ts
@@ -1,1 +1,8 @@
-type GetMiddleElement<T> = any
+type GetMiddleElement<T, Agr extends unknown[] = []> =
+T extends [infer L, ...infer R]
+  ? R['length'] extends [...Agr, L]['length']
+    ? [[...Agr, L][Agr['length']], R[0]]
+    : R['length'] extends Agr['length']
+      ? [L]
+      : GetMiddleElement<R, [...Agr, L]>
+  : T
