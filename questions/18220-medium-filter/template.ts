cdc63aa4d1fa24bbf3c6b96a01a079a9fe6663e4
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-20 17:48:52 +0100

    18220

--- questions/18220-medium-filter/template.ts
+++ questions/18220-medium-filter/template.ts
@@ -1,1 +1,8 @@
-type Filter<T extends any[], P> = []
+type Filter<T extends any[], P> =
+T extends [infer L, ... infer R]
+  ? L extends P
+    ? [L, ...Filter<R, P>]
+    : [...Filter<R, P>]
+  : T
+
+type X = Filter<[0, 1, 2], 2> // [2]
