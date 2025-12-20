14728379dfaa2baddc912c86c70f978074371d4b
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-20 15:15:48 +0100

    16259 to primitive

--- questions/16259-medium-to-primitive/template.ts
+++ questions/16259-medium-to-primitive/template.ts
@@ -1,1 +1,7 @@
-type ToPrimitive = any
+type ToPrimitive<T> = T extends object ? (
+  T extends (...args: unknown[]) => unknown ? Function : {
+    [K in keyof T]: ToPrimitive<T[K]>
+  }
+) : (
+  T extends { valueOf: () => infer P } ? P : T
+)
