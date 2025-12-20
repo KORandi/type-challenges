36be7335cdfedbf1d5fab1519edf85dcbd2a7fb7
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-20 23:47:15 +0100

    triangular

--- questions/27152-medium-triangular-number/template.ts
+++ questions/27152-medium-triangular-number/template.ts
@@ -1,1 +1,6 @@
-type Triangular<N extends number> = any
+type Triangular<N extends number, Counter extends unknown[] = [], Result extends unknown[] = []> =
+Counter['length'] extends N
+  ? Result['length']
+  : [unknown, ...Counter] extends infer C extends unknown[]
+      ? Triangular<N, C, [...Result, ...ConstructTuple<C['length']>]>
+      : never
