38ec6fc1a7e9da6dfa0a82afee984aa3b461bd3a
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-20 20:50:06 +0100

    21220 Permutation of tuple

--- questions/21220-medium-permutations-of-tuple/template.ts
+++ questions/21220-medium-permutations-of-tuple/template.ts
@@ -1,1 +1,8 @@
-type PermutationsOfTuple<T extends unknown[]> = any
+type PermutationsOfTuple<T extends unknown[]> =
+T extends [infer L, ...infer R]
+  ? R extends [infer LL, ...infer Rest]
+    ? [L, ...PermutationsOfTuple<R>] | [LL, ...PermutationsOfTuple<[L, ...Rest]>] | [...PermutationsOfTuple<[L, ...Rest]>, LL] | [...PermutationsOfTuple<R>, L]
+    : [L, ...PermutationsOfTuple<R>] | [...PermutationsOfTuple<R>, L]
+  : []
+
+type X = PermutationsOfTuple<[1, number, unknown]>
