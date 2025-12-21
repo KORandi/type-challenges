e0ad8eaa14a1ddf66e8c69b09c65a4585cc9493e
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-21 15:35:53 +0100

    29650

--- questions/29650-medium-extracttoobject/template.ts
+++ questions/29650-medium-extracttoobject/template.ts
@@ -1,1 +1,10 @@
-type ExtractToObject<T, U> = any
+type ExtractToObject<T extends Record<PropertyKey, unknown>, U extends PropertyKey> =
+  T[U] extends Record<PropertyKey, unknown>
+    ? {
+        [K in keyof Omit<T, U> | keyof T[U]]: K extends keyof T
+          ? T[K]
+          : K extends keyof T[U]
+            ? T[U][K]
+            : never
+      }
+    : U
