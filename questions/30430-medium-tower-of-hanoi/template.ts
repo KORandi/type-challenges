64cd4a5d8ef2228e59700a05e3daf36f65fc7a79
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-21 17:05:33 +0100

    30430

--- questions/30430-medium-tower-of-hanoi/template.ts
+++ questions/30430-medium-tower-of-hanoi/template.ts
@@ -1,1 +1,13 @@
-type Hanoi<N extends number, From = 'A', To = 'B', Intermediate = 'C'> = any
+type Hanoi<
+  N extends number,
+  From = 'A',
+  To = 'B',
+  Intermediate = 'C',
+  Counter extends 1[] = [],
+> = Counter['length'] extends N
+  ? []
+  : [
+      ...Hanoi<N, From, Intermediate, To, [...Counter, 1]>,
+      [From, To],
+      ...Hanoi<N, Intermediate, To, From, [...Counter, 1]>,
+    ]
