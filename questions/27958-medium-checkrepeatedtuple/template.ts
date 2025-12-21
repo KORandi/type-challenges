a797c4558a5f53420cf27fc0d12cc216afb7f11c
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-21 12:32:03 +0100

    check repeated tuple

--- questions/27958-medium-checkrepeatedtuple/template.ts
+++ questions/27958-medium-checkrepeatedtuple/template.ts
@@ -1,1 +1,13 @@
-type CheckRepeatedTuple<T extends unknown[]> = any
+type IsInTuple<T, Tuple extends unknown[]> =
+  Tuple extends [infer First, ...infer Rest]
+    ? IsEqual<T, First> extends true
+      ? true
+      : IsInTuple<T, Rest>
+    : false
+
+type CheckRepeatedTuple<T extends unknown[], Visited extends unknown[] = []> =
+  T extends [infer L, ...infer R]
+    ? IsInTuple<L, Visited> extends true
+      ? true
+      : CheckRepeatedTuple<R, [...Visited, L]>
+    : false
