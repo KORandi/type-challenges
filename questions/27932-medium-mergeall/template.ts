cbcc00346c1d3151e3df3ff2732f58e060de1550
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-21 12:19:49 +0100

    merge all

--- questions/27932-medium-mergeall/template.ts
+++ questions/27932-medium-mergeall/template.ts
@@ -1,1 +1,14 @@
-type MergeAll<XS> = any
+type MergeAll<XS> =
+XS extends [infer L extends Record<any, any>, ...infer R extends Record<any, any>[]]
+  ? MergeAll<R> extends infer S
+    ? {
+        [K in keyof L | keyof S]: K extends keyof L
+          ? K extends keyof S
+            ? L[K] | S[K]
+            : L[K]
+          : K extends keyof S
+            ? S[K]
+            : never
+      }
+    : never
+  : {}
