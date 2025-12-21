ae8c1c7e3aa43f706e4879a7655eb7a29bf7bcb0
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-21 16:35:33 +0100

    30301

--- questions/30301-medium-isodd/template.ts
+++ questions/30301-medium-isodd/template.ts
@@ -1,1 +1,5 @@
-type IsOdd<T extends number> = any
+type IsOdd<T extends number> =
+  number extends T ? false :
+  `${T}` extends `${string}${1 | 3 | 5 | 7 | 9}` ?
+    `${T}` extends `${string}${'e' | 'E' | '.'}${string}` ? false : true
+    : false
