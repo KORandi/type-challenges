eb8cedd0a17453d82178466d80cac74bd0a75883
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-21 21:51:38 +0100

    30958

--- questions/30958-medium-pascals-triangle/template.ts
+++ questions/30958-medium-pascals-triangle/template.ts
@@ -1,1 +1,18 @@
-type Pascal<N extends number> = any
+type PasscalAdjCal<T extends number[], Prev extends number = 0> =
+T extends [infer L extends number, ...infer R extends number[]]
+  ? Prev extends 0
+    ? PasscalAdjCal<R, L>
+    : [[...ConstructTuple<Prev>, ...ConstructTuple<L>]['length'], ...PasscalAdjCal<R, L>]
+  : []
+
+type PascalImpl<N extends number, PreviousPascal extends number[] = []> =
+N extends 0
+  ? [1]
+  : [1, ...PasscalAdjCal<PreviousPascal>, 1]
+
+type Pascal<N extends number, Counter extends unknown[] = [], PreviousPascal extends number[] = []> =
+Counter['length'] extends N
+  ? []
+  : PascalImpl<Counter['length'], PreviousPascal> extends infer PS extends number[]
+    ? [PS, ...Pascal<N, [...Counter, unknown], PS>]
+    : never
