8a5bec6cb981f709d57b7c59108d1c0c297841d5
Konstantin Kozokar <konstantin@neotalogic.com>
2025-12-21 15:54:13 +0100

    29785

--- questions/29785-medium-deep-omit/template.ts
+++ questions/29785-medium-deep-omit/template.ts
@@ -1,1 +1,11 @@
-type DeepOmit = any
+type DeepOmit<T, U extends string> =
+U extends `${infer L extends string}.${infer R extends string}`
+  ? { [K in keyof T]: K extends L
+      ? DeepOmit<T[K], R>
+      : T[K] }
+  : U extends `${infer L extends string}`
+    ?
+      {
+        [K in keyof Omit<T, L>]: T[K]
+      }
+    : never
